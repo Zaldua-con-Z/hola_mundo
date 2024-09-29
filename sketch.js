d7fb6fffd094aadea697cb87c4586b3e4ee5ec8c
Zaldua-con-Z <camila.zaldua@ucuenca.edu.ec>
2024-09-29 15:00:39 -0500

    Se creó un cursor bien bonito y chévere

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	// frameRate(24);
+	background(0);
+	rectMode(CENTER);
+}
+
+function draw() {
+	background(0, random(0,100));
+
+	noStroke()
+    fill(255,255,0)
+    let biteSize=PI/8
+    let startAngle=biteSize*sin(frameCount*0.1)+biteSize
+    let endAngle=TWO_PI-startAngle
+    arc(mouseX, mouseY,100,100,startAngle,endAngle,PIE)
+    
+    
+    
+    noStroke
+    fill(0,0,255)
+    circle(mouseX+80,mouseY,20,20)
+
+    noStroke
+    fill(150,0,152)
+    circle(mouseX+120,mouseY,20,20)
+
+    noStroke
+    fill(255,0,0)
+    circle(mouseX+160,mouseY,20,20)
+}
